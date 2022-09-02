b55a6e4f2c33e25082d33719b5f5c7d1dfddd6ee
marwan mostafa ghaleb ghalab <58035531+MarwanaMostafa@users.noreply.github.com>
2022-09-03 01:07:57 +0200

    Update T1.kt

--- Task1/T1.kt
+++ Task1/T1.kt
@@ -1,1 +1,25 @@
 
+
+fun main(args :Array<String>) {
+	val num1 =args[0].toInt()
+	val num2 =args[2].toInt()
+	val operation =args[1]
+	var result =0
+    if(operation =="+"){
+        result=num1+num2
+		println("$num1 + $num2 = $result")}
+
+    else if(operation =="-")
+    {
+        result=num1-num2
+		println("$num1 - $num2 = $result")}
+
+    else if(operation =="*")
+     {
+        result=num1*num2
+		println("$num1 * $num2 = $result")}
+    else if(operation =="/")
+ {
+        result=num1/num2
+		println("$num1 / $num2 = $result")}
+}
