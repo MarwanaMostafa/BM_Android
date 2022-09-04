7fdc15f5dec91ee8933b6970b466e2ccffdb8549
marwan mostafa ghaleb ghalab <58035531+MarwanaMostafa@users.noreply.github.com>
2022-09-05 00:06:54 +0200

    Create MatchWithCount.kt

---
+++ Task2/MatchWithCount.kt
@@ -0,0 +1,20 @@
+//I want to return true if at least one item matches the following conditions:
+//1. The item length is >= 'count'
+//Otherwise, return false.
+//Hint: if items = ["a", "abc"] and count = 3, will return true.
+
+fun MatchWithCount(list: List<String>,count:Int) :Boolean{
+    val result: MutableList<String> = mutableListOf("") 
+    for (i in list) 
+        if (i.length>=count) 
+        	return true 
+    return false
+
+}
+fun main(args: Array<String>) {
+	    println(MatchWithCount(listOf("M.", "Maze", "More"),args[0].toInt() ) )
+
+        
+           
+        
+}
